50f10514af00c2fe421c13e88570cc3217245c0f
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-16 18:09:12 -0700

    Created a Student struct, continue setting the main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,18 +1,37 @@
-// COMSC-210 | Lab 11 | Dat Hoang Vien
+// COMSC-210 | Lab 12 | Dat Hoang Vien
 // IDE used: Visual Code Studio
 
 // Libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include "Student.h"
+using namespace std;
 
 // Constants
+const int ARRAY_SIZE = 100;
 
 // Function prototypes
+void fileInput(string path);
 
 // Main
+int main()
+{
+    // Declare an array to store Student objects data
+    array<Student, ARRAY_SIZE>;
+
+    return 0;
+}
 
 // Function implementations
 /*
-    addStudent()
-    Add a new Student object to the existing array
-    Arguments: a Student array pointer, a count integer to keep track of the array size
+    fileInput()
+    Add new Student objects from a file
+    Arguments: the path to the data file
     Returns: none
 */
+void fileInput(string path)
+{
+    
+}
