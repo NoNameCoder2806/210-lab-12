664526abe8d7ba21050aa4ff23ea2ef8c1c8ea71
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-16 17:58:08 -0700

    Initializing the Repository

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// COMSC-210 | Lab 11 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+// Libraries
+
+// Constants
+
+// Function prototypes
+
+// Main
+
+// Function implementations
+/*
+    addStudent()
+    Add a new Student object to the existing array
+    Arguments: a Student array pointer, a count integer to keep track of the array size
+    Returns: none
+*/
