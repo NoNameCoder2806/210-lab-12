96db9dbd5895aebd0ccebe1a9b765f86230f6122
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-16 18:40:12 -0700

    Implemented the displayArray function, added 2 constants: NAME_FIELD and GPA_FIELD

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 // Libraries
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <array>
 #include "Student.h"
@@ -11,9 +12,13 @@ using namespace std;
 
 // Constants
 const int ARRAY_SIZE = 100;
+const int NAME_FIELD = 30;
+const int GPA_FIELD = 5;
 
 // Function prototypes
-void fileInput(array<Student, ARRAY_SIZE> studentArray, string path, int& count);
+void fileInput(array<Student, ARRAY_SIZE>& studentArray, string path, int& count);
+
+void displayArray(const array<Student, ARRAY_SIZE>& studentArray, const int count);
 
 // Main
 int main()
@@ -22,6 +27,16 @@ int main()
     array<Student, ARRAY_SIZE> studentArray;
     int count = 0;      // Store the total number of Student objects in the array
 
+    // Read the data from the 2 files
+    fileInput(studentArray, "data1.txt", count);
+    fileInput(studentArray, "data2.txt", count);
+
+    // Print the current size
+    cout << "Current size: " << count << endl;
+    
+    // Display all the Student objects in the array
+    displayArray(studentArray, count);
+
     return 0;
 }
 
@@ -29,10 +44,13 @@ int main()
 /*
     fileInput()
     Add new Student objects from a file
-    Arguments: the path to the data file
+    Arguments: 
+        - studentArray: reference to the std::array<Student, ARRAY_SIZE> that will store the data
+        - path: path to the data file
+        - count: reference to an int tracking how many slots are currently used
     Returns: none
 */
-void fileInput(array<Student, ARRAY_SIZE> studentArray, string path, int& count)
+void fileInput(array<Student, ARRAY_SIZE>& studentArray, string path, int& count)
 {
     // Create a file stream and read the file
     ifstream fin;
@@ -73,5 +91,24 @@ void fileInput(array<Student, ARRAY_SIZE> studentArray, string path, int& count)
         // Add the object into the array
         studentArray[count] = temp;
         count++;      // Increment the count
+    }
+}
+
+/*
+    displayArray()
+    Display the data of the Student objects in the array
+    Arguments: an array 
+    Returns: none
+*/
+void displayArray(const array<Student, ARRAY_SIZE>& studentArray, const int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        // Display the fields
+        cout << left << setw(NAME_FIELD) << studentArray[i].name;
+        cout << left << setw(GPA_FIELD) << studentArray[i].gpa;
+
+        // Enter a new line
+        cout << endl;
     }
 }
